4660fcac92957294626273cc734a19ad364ed000
Roy Fochtman <roy.fochtman@hotmail.com>
2012-10-05 15:37:46 +0200

    Next changes

---
+++ src/main/java/components/Kamera.java
@@ -0,0 +1,12 @@
+package components;
+
+abstract class Kamera {
+	
+	public String marke;
+	public String modell;
+	public int gewicht;
+	public String objektivanschluss;
+	public int bildwinkel;
+	public double aufloesung;
+	public String bildsensor;
+}
